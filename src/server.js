421e1e15fb823fe2fa67110da5ea1aab5daac717
codeer <loginonlyyy@gmail.com>
2022-01-11 22:48:20 +0900

    GET requests and responses

--- src/server.js
+++ src/server.js
@@ -4,7 +4,19 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("This is Home Page");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+
+app.get("/", handleHome);
+
+app.get("/login", handleLogin);
+
 const handleListening = () =>
-  console.log(`🐱‍🏍Server listening on http://local:${PORT}`);
+  console.log(`🚀 Server listening on http://local:${PORT}`);
 
 app.listen(PORT, handleListening);
