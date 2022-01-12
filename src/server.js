3de654780f618bc2b2162a33e83448c20bd31f17
codeer <loginonlyyy@gmail.com>
2022-01-12 16:24:26 +0900

    installed morgan

--- src/server.js
+++ src/server.js
@@ -1,25 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("This is Home Page");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here");
-};
-
 app.use(logger);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`🚀 Server listening on http://localhost:${PORT}`);
