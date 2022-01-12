75c53db224d431a2d758158cda410e5ab98c75e3
codeer <loginonlyyy@gmail.com>
2022-01-12 15:24:22 +0900

    created a logger middleware

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("This is Home Page");
 };
@@ -12,11 +17,11 @@ const handleLogin = (req, res) => {
   return res.send("Login here");
 };
 
+app.use(logger);
 app.get("/", handleHome);
-
 app.get("/login", handleLogin);
 
 const handleListening = () =>
-  console.log(`🚀 Server listening on http://local:${PORT}`);
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
