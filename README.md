7031ef6542eace850922ff2f6a544e89a7a6fd48
codeer <loginonlyyy@gmail.com>
2022-01-16 18:12:02 +0900

    created more urls and controllers

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # youtube-clone-2021
 
 <img src="https://img.shields.io/badge/JavaScript-v14.15.1-blue"/>
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/remove -> Remove MY Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/remove -> Remove Video
+/videos/upload -> Upload Video
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("Trending");
-export const watch = (req, res) => res.send("Watch video");
+export const search = (req, res) => res.send("Search video");
+export const upload = (req, res) => res.send("Upload video");
 export const edit = (req, res) => res.send("Edit video");
+export const see = (req, res) => res.send("See video");
+export const remove = (req, res) => res.send("Remove video");
