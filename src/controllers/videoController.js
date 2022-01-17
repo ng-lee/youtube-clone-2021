478a7c83930b3b6138fb2d23002bc6aeb3bd063f
codeer <loginonlyyy@gmail.com>
2022-01-18 01:37:59 +0900

    usage of pug variables and extend

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search video");
 export const upload = (req, res) => res.send("Upload video");
-export const edit = (req, res) => res.send("Edit video");
-export const see = (req, res) => res.send("See video");
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const remove = (req, res) => res.send("Remove video");
