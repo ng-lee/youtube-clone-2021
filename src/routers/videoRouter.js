5a0b6753419938eec06a4e7103a432f288e6e85d
codeer <loginonlyyy@gmail.com>
2022-01-20 01:03:42 +0900

    post method in edit page

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { upload, watch, edit, remove } from "../controllers/videoController";
+import {
+  upload,
+  watch,
+  getEdit,
+  postEdit,
+  remove,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/remove", remove);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
