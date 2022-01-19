9027d7165e52eee50a95507d812ee8d052cb9dd3
codeer <loginonlyyy@gmail.com>
2022-01-20 00:07:02 +0900

    practice with fake DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,38 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 3,
+    createdAt: "9 minutes ago",
+    views: 24,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 2,
+    comments: 5,
+    createdAt: "7 minutes ago",
+    views: 5,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Hello",
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Whatsup",
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const search = (req, res) => res.send("Search video");
+export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload video");
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const remove = (req, res) => res.send("Remove video");
