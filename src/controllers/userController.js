23d950f1283659421a227de7e80a0564913e5c4c
codeer <loginonlyyy@gmail.com>
2022-02-02 00:21:56 +0900

    added login info to session

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,7 +44,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
-  console.log("LOG USER IN!");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit user");
