d467a9b5ad2ecbf2185144aa44561c2616b156c7
codeer <loginonlyyy@gmail.com>
2022-02-03 15:35:03 +0900

    github login part Six

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -99,7 +99,26 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    const email = emailData.find((email) => email.primary && email.verified);
+    const emailObj = emailData.find((email) => email.primary && email.verified);
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+    }
     return res.redirect("/");
   } else {
     return res.redirect("/login");
