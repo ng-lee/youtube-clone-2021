429af70cf4895dbfafe4981759673feea1ea8e58
codeer <loginonlyyy@gmail.com>
2022-02-04 15:07:27 +0900

    loginOnly and publicOnly Middleware

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,14 @@ import {
   postEdit,
   see,
 } from "../controllers/userController";
+import { loginOnlyMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", loginOnlyMiddleware, logout);
+userRouter.route("/edit").all(loginOnlyMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware,startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware,finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
