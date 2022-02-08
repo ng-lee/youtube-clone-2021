40d8e3316a6fc507061c8654ab9d842958230814
codeer <loginonlyyy@gmail.com>
2022-02-08 12:33:27 +0900

    video owner part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,4 @@
+import User from "../models/User";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
@@ -8,10 +9,11 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  const owner = await User.findById(video.owner);
   if (!video) {
     res.status(404).render("404", { pageTitle: "Video not found" });
   }
-  res.render("watch", { pageTitle: video.title, video });
+  res.render("watch", { pageTitle: video.title, video, owner });
 };
 
 export const getEdit = async (req, res) => {
@@ -43,14 +45,20 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
-  const { path: fileUrl } = req.file;
-  const { title, description, hashtags } = req.body;
+  const {
+    body: { title, description, hashtags },
+    sessoin: {
+      user: { _id },
+    },
+    file: { path: fileUrl },
+  } = req;
   try {
     await Video.create({
       title,
       fileUrl,
       description,
       hashtags: Video.formatHashtags(hashtags),
+      owner: _id,
     });
   } catch (error) {
     return res.status(400).render("upload", {
