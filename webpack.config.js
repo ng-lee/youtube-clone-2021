095b74d9d691e6ec12091e3c330caa74dd42e590
codeer <loginonlyyy@gmail.com>
2022-02-10 15:16:33 +0900

    more cofigurations

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ module.exports = {
   entry: "./src/client/js/main.js",
   plugins: [new MiniCssExtractPlugin({ filename: "css/styles.css" })],
   mode: "development",
+  watch: true,
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
