a88a58cafabbf9d0724e6a6da1799336c90e672e
codeer <loginonlyyy@gmail.com>
2022-02-15 19:55:25 +0900

    mute and unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,14 +2,19 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 playBtn.addEventListener("click", (event) => {
   if (video.paused) {
-    playBtn.innerText = "Pause";
     video.play();
   } else {
-    playBtn.innerText = "Play";
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Pause" : "Play";
+});
+
+muteBtn.addEventListener("click", () => {
+  video.muted = !video.muted;
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
 });
