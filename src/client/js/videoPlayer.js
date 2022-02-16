7004887318dfdeb55e7cfad790d34973964893e8
codeer <loginonlyyy@gmail.com>
2022-02-16 18:11:31 +0900

    fullscreen

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,6 +5,8 @@ const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -54,4 +56,15 @@ timeline.addEventListener("input", (event) => {
     target: { value },
   } = event;
   video.currentTime = value;
+});
+
+fullScreenBtn.addEventListener("click", () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = "Enter Fullscreen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = "Exit Fullscreen";
+  }
 });
