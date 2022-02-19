b28cf39b515e3a182d51688c43a4ea8a48550cb4
codeer <loginonlyyy@gmail.com>
2022-02-19 15:51:23 +0900

    use sendStatus instead of status

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -115,11 +115,11 @@ export const search = async (req, res) => {
 
 export const registerView = async (req, res) => {
   const { id } = req.params;
-  const video = Video.findById(id);
+  const video = await Video.findById(id);
   if (!video) {
-    return res.status(404);
+    return res.sendStatus(404);
   }
   video.meta.views = video.meta.views + 1;
   await video.save();
-  return res.status(200);
+  return res.sendStatus(200);
 };
