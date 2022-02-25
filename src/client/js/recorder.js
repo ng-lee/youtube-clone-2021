1e0a73e0f66513c32a77c1e21a06a6f3401c458a
codeer <loginonlyyy@gmail.com>
2022-02-25 13:00:29 +0900

    recording video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,37 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
-startBtn.addEventListener("click", async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log(event);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
   preview.srcObject = stream;
   preview.play();
-});
+};
+
+init();
+
+startBtn.addEventListener("click", handleStart);
