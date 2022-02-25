0fd7cf2cf67972f444fda4b61a2ae457dd8abb3c
codeer <loginonlyyy@gmail.com>
2022-02-25 12:31:34 +0900

    video preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,11 @@
 const startBtn = document.getElementById("startBtn");
+const preview = document.getElementById("preview");
 
 startBtn.addEventListener("click", async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  preview.srcObject = stream;
+  preview.play();
 });
