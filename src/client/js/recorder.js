ebae2d00de592fc7105f4af612ee956dcf7ddf15
codeer <loginonlyyy@gmail.com>
2022-02-25 17:10:08 +0900

    unlink and revoke for memory

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -12,7 +12,15 @@ const handleDownload = async () => {
   await ffmpeg.load();
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
-  await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
   const mp4File = ffmpeg.FS("readFile", "output.mp4");
   const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
@@ -31,6 +39,14 @@ const handleDownload = async () => {
   thumbA.download = "Mythumbnail!.jpg";
   document.body.appendChild(thumbA);
   thumbA.click();
+
+  ffmpeg.FS("unlink", "recording.webm");
+  ffmpeg.FS("unlink", "output.mp4");
+  ffmpeg.FS("unlink", "thumbnail.jpg");
+
+  URL.revokeObjectURL(mp4URL);
+  URL.revokeObjectURL(thumbURL);
+  URL.revokeObjectURL(videoFile);
 };
 
 const handleStop = () => {
