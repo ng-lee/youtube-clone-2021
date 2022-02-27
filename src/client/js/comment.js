5e1dcc2705f74262a0d8614f7d2eedc58935f576
codeer <loginonlyyy@gmail.com>
2022-02-27 15:50:59 +0900

    realtime comments

--- src/client/js/comment.js
+++ src/client/js/comment.js
@@ -3,18 +3,30 @@ const form = document.getElementById("commentForm");
 const textarea = form.querySelector("textarea");
 const btn = form.querySelector("button");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const span = document.createElement("span");
+  span.innerText = text;
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const text = textarea.value;
   const { id: videoId } = videoContainer.dataset;
   if (text === "") return;
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+  if (status == 201) {
+    addComment(text);
+  }
   textarea.value = "";
-  window.location.reload();
 });
