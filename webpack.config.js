571739acbbb7df03fc71364bf154e36aed8ddf1d
codeer <loginonlyyy@gmail.com>
2022-03-01 14:54:55 +0900

    building the frontend

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
     comment: `${BASE_JS}/comment.js`,
   },
   plugins: [new MiniCssExtractPlugin({ filename: "css/styles.css" })],
-  mode: "development",
-  watch: true,
   output: {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
